79b3505b5704a552ef581ea276ef8aac90a1f3c9
Luc9718 <142433756+Luc9718@users.noreply.github.com>
2023-08-17 14:13:13 -0300

    Update app.py

--- APP/app.py
+++ APP/app.py
@@ -5,27 +5,24 @@ import numpy as np
 st.title("Web App Football Data")
 
 st.sidebar.header("Leagues")
-selected_league = st.sidebar.selectbox('League',['England'])
+selected_league = st.sidebar.selectedbox('League', ['England','Germany','Italy','Spain','France'])
 
 st.sidebar.header("Season")
-selected_season = st.sidebar.selectbox('League',['2022/2023'])
+selected_season = st.sidebar.selectedbox('Season', ['2023,2022','2022/2021','2021/2020'])
 
 # WebScraping Football Data
 def load_data(league, season):
-  
-  if selected_league == 'England':
-    league = 'E0'    
-
-if selected_season == '2022/2023':
-  season = '2223'
-  
-  url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
+  url = https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv'
   data = pd.read_csv(url)
   return data
 
-df = load_data(select_league, select_season)
-  
-st.subheader("Dataframe: "+selected_league)
-st.dataframe(df)
+df = load_data(selected_league, selected_season)
+
+
+
+
+
+
+
 
 
